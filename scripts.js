ca89cfea404647ca7e86b5e2a0043a46382e1d83
bashtest <test.bash@mail.ru>
2015-02-22 02:54:56 +0500

    авторегулировка высоты
    если получится :satisfied:

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 $(function() {
-    var main = {
-        page: ko.observable('start')
-    }
+
+    VK.init();
+
+    var main = {};
+    main.page = ko.observable('start');
+    main.page.subscribe(function(){
+        VK.callMethod('resizeWindow', {
+            width: document.body.offsetWidth,
+            height: document.body.clientHeight
+        });
+    });
 
     ko.applyBindings(main, document.body);
 });
